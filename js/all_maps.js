3ae31450c90c8a0389cc1e135d1bb23fb62a900c
Chase <kacmlweimer@gmail.com>
2016-10-04 22:56:00 -0500

    added so many freaking maps

--- js/all_maps.js
+++ js/all_maps.js
@@ -7,6 +7,9 @@ var allMaps = {
 	cave1: new Map(gameData, createPebble),
 	cave2: new Map(gameData, createPebble),
 	forest: new Map(gameData, createGrass),
+	forest1: new Map(gameData, createGrass),
+	forest2: new Map(gameData, createGrass),
+	forest3: new Map(gameData, createGrass),
 	grass: new Map(gameData, createGrass),
 	grass1: new Map(gameData, createGrass),
 	grass2: new Map(gameData, createGrass),
@@ -16,6 +19,10 @@ var allMaps = {
 	desert1: new Map(gameData, createSand),
 	desert2: new Map(gameData, createSand),
 	cliff: new Map(gameData, createPebble),
+	ice: new Map(gameData, createSand),
+	ice1: new Map(gameData, createSand),
+	ice2: new Map(gameData, createSand),
+	ice3: new Map(gameData, createSand)
 };
 
 var doors = {
@@ -56,9 +63,9 @@ allMaps.grass.surrounding_maps["n"] = getGrassMap1;
 allMaps.grass.surrounding_maps["e"] = getGrassMap2;
 allMaps.grass.surrounding_maps["w"] = getEntranceMap;
 
+allMaps.grass1.surrounding_maps["n"] = getIceMap;
 allMaps.grass1.surrounding_maps["e"] = getGrassMap3;
 allMaps.grass1.surrounding_maps["s"] = getGrassMap;
-allMaps.grass1.surrounding_maps["w"] = getCaveMap;
 
 allMaps.grass2.surrounding_maps["n"] = getGrassMap3;
 allMaps.grass2.surrounding_maps["w"] = getGrassMap;
@@ -68,7 +75,7 @@ allMaps.grass3.surrounding_maps["s"] = getGrassMap2;
 allMaps.grass3.surrounding_maps["w"] = getGrassMap1;
 
 allMaps.grass4.surrounding_maps["s"] = getGrassMap3;
-// allMaps.grass4.surrounding_maps["w"] = getIceMap;
+allMaps.grass4.surrounding_maps["w"] = getIceMap;
 
 allMaps.desert.surrounding_maps["e"] = getEntranceMap;
 allMaps.desert.surrounding_maps["w"] = getDesertMap1;
@@ -81,7 +88,29 @@ allMaps.desert2.surrounding_maps["n"] = getCliffMap;
 
 allMaps.cliff.surrounding_maps["s"] = getDesertMap2;
 
-allMaps.forest.surrounding_maps["s"] = getStartMap;
+allMaps.forest.surrounding_maps["e"] = getIceMap1;
+allMaps.forest.surrounding_maps["w"] = getForestMap1;
+
+allMaps.forest1.surrounding_maps["e"] = getForestMap;
+allMaps.forest1.surrounding_maps["w"] = getForestMap2;
+
+allMaps.forest2.surrounding_maps["n"] = getForestMap3;
+allMaps.forest2.surrounding_maps["e"] = getForestMap1;
+
+allMaps.forest3.surrounding_maps["s"] = getForestMap2;
+
+allMaps.ice.surrounding_maps["n"] = getIceMap1;
+allMaps.ice.surrounding_maps["e"] = getGrassMap4;
+
+allMaps.ice1.surrounding_maps["e"] = getIceMap2;
+allMaps.ice1.surrounding_maps["s"] = getIceMap;
+allMaps.ice1.surrounding_maps["w"] = getForestMap;
+
+allMaps.ice2.surrounding_maps["n"] = getIceMap3;
+allMaps.ice2.surrounding_maps["w"] = getIceMap1;
+
+allMaps.ice3.surrounding_maps["s"] = getIceMap2;
+
 
 
 // TODO: make a general "initMap" function w/ a callback for adding crap
@@ -166,13 +195,42 @@ function getForestMap() {
 	var m = allMaps.forest;
 	if (!m.initialized) {
 		m.init();
-		m.coverRegionWithTile(0, 0, 17, 1, createShrubbery, false);
-		m.coverRegionWithTile(0, 1, 1, 15, createShrubbery, false);
-		m.coverRegionWithTile(16, 1, 1, 15, createShrubbery, false);
 
 		m.addItemAtPoint(createWolf(), { x: 5, y: 10 });
 		m.addItemAtPoint(createWolf(), { x: 10, y: 10 });
 		m.addItemAtPoint(createWolf(), { x: 3, y: 8 });
+	}
+	return m;
+}
+
+function getForestMap1() {
+	var m = allMaps.forest1;
+	if (!m.initialized) {
+		m.init();
+
+		m.addItemAtPoint(createWolf(), { x: 4, y: 8 });
+	}
+	return m;
+}
+
+function getForestMap2() {
+	var m = allMaps.forest2;
+	if (!m.initialized) {
+		m.init();
+
+		m.addItemAtPoint(createKnight(), { x: 5, y: 10 });
+		
+	}
+	return m;
+}
+
+function getForestMap3() {
+	var m = allMaps.forest3;
+	if (!m.initialized) {
+		m.init();
+
+		m.addItemAtPoint(createKnight(), { x: 2, y: 10 });
+		
 	}
 	return m;
 }
@@ -287,6 +345,50 @@ function getCliffMap() {
 		m.init();
 
 		m.addItemAtPoint(createXpPotion(), { x: 5, y: 10 });
+		
+	}
+	return m;
+}
+
+function getIceMap() {
+	var m = allMaps.ice;
+	if (!m.initialized) {
+		m.init();
+
+		m.addItemAtPoint(createHealthPotion(), { x: 2, y: 9 });
+		
+	}
+	return m;
+}
+
+function getIceMap1() {
+	var m = allMaps.ice1;
+	if (!m.initialized) {
+		m.init();
+
+		m.addItemAtPoint(createCoin(), { x: 5, y: 9 });
+		
+	}
+	return m;
+}
+
+function getIceMap2() {
+	var m = allMaps.ice2;
+	if (!m.initialized) {
+		m.init();
+
+		m.addItemAtPoint(createCoin(), { x: 6, y: 13 });
+		
+	}
+	return m;
+}
+
+function getIceMap3() {
+	var m = allMaps.ice3;
+	if (!m.initialized) {
+		m.init();
+
+		m.addItemAtPoint(createShrubbery(), { x: 2, y: 13 });
 		
 	}
 	return m;
